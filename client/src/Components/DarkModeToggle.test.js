c95b8c0778e37a418c92c84111ba7b3541e12780
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-10 12:43:50 -0500

    Added the test and have all of them passing. Going to make it fail to make sure it works right.

--- client/src/Components/DarkModeToggle.test.js
+++ client/src/Components/DarkModeToggle.test.js
@@ -6,8 +6,10 @@ test('DarkModeToggle renders', () => {
     render(<DarkModeToggle />);
 });
 test('Dark Mode button clicks', () => {
+    const { getByText } = render(<DarkModeToggle />);
     fireEvent.click(getByText(/dark Mode/i));
 });
 test('Light Mode button clicks', () => {
+    const { getByText } = render(<DarkModeToggle />);
     fireEvent.click(getByText(/Light Mode/i));
 });
