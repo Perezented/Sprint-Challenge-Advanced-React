e68f1b1d47195d09dbcfac0766478c5ab107a350
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-10 12:49:47 -0500

    Added some styles to separate each player.

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import './Card.css';
 const Card = (props) => {
     props.players.map((value) => console.log(value));
     return (
         <div>
             {props.players.map((value) => {
                 return (
-                    <section>
+                    <section className="box">
                         <h2>{value.name}</h2>
-                        <div>
+                        <div className="flexMe">
                             <p>Country: {value.country}</p>
                             <p>Searches: {value.searches}</p>
                             <p>Number: {value.id}</p>
