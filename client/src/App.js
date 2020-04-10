b8a1e4493f1c6b6538e51778e9b0dfcbed2223e0
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-10 11:18:41 -0500

    Switched App.js over to a class, breaking the switch... Rendered each soccer player with a bit of information for them.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Card from './Components/Card';
+import Switch from './Components/Switch';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            players: [],
+            check: false,
+        };
+    }
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/players').then((response) => {
+            console.log(response.data);
+            this.setState({
+                players: response.data,
+            });
+        });
+    }
+    //Built a switch to toggle darkMode
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <h1>Advanced React</h1>
+                    {/* Displaying of said Switch in the header area */}
+                    <Switch isOn={this.state.check} />
+                </header>
+                <section>
+                    {/* A div to hold the players at some point */}
+                    <Card players={this.state.players} />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
